e5993ec0369c86231169feeb9be886b50aebdd66
jjayoma2017 <jjayoma2017@gmail.com>
2022-07-08 10:01:54 +0800

    added code for events in post and comments

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,26 +1,40 @@
-const express = require("express");
-const { randomBytes } = require("crypto");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+const axios = require('axios');
 
 const commentsByPostId = {};
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts/:id/comments", (req, res) => {
+app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
-  const id = randomBytes(4).toString("hex");
+app.post('/posts/:id/comments', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
   const comments = commentsByPostId[req.params.id] || [];
   const { content } = req.body;
   comments.push({ id, content });
   commentsByPostId[req.params.id] = comments;
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id,
+      content,
+      postId: req.params.id,
+    },
+  });
   res.status(201).send(comments);
 });
 
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+  res.send({});
+});
+
 app.listen(4001, () => {
-  console.log("Comments. Listening on port 4001");
+  console.log('Comments. Listening on port 4001');
 });
