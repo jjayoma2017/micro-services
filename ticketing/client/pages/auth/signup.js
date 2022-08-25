bb87252de7b75a2649caef76153e5ecd8fe0e79e
jjayoma2017 <jjayoma2017@gmail.com>
2022-08-25 11:22:06 +0800

    added ssr

---
+++ ticketing/client/pages/auth/signup.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
+
+export default function signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: (data) => {
+      console.log(data);
+      Router.push('/');
+    },
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await doRequest();
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          type="text"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+}
